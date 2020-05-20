e2970c089edba3027d7c575c59f58faca9515cb7
Ricardo Angeli <rangeli@fetchrobotics.com>
2020-05-20 17:59:28 -0400

    Implemented improved version of getting values using keys

--- viper-test.go
+++ viper-test.go
@@ -23,40 +23,24 @@ func initConfig(conf string) error {
 	return nil
 }
 
-func getTestyManagerConfig() *Config {
-	cmds := viper.GetStringMap("testy_manager.cmds")
-	testy, _ := cmds["testy"].(map[string]interface{})
-	// if !ok {
-	// 	return fmt.Errorf("Error parsing testy_manager")
-	// }
-
-	sliceInterfacesToSliceStrings := func(values []interface{}) []string {
-		var output []string
-		for _, val := range values {
-			output = append(output, val.(string))
+func getTestyManagerConfigs() []Config {
+	var configs []Config
+	for key := range viper.GetStringMap("testy_manager.cmds") {
+		cfg := Config{
+			Directories:    viper.GetStringSlice("testy_manager.cmds." + key + ".upload_dir"),
+			SearchInterval: viper.GetDuration("testy_manager.cmds." + key + ".search_interval"),
+			Command:        viper.GetString("testy_manager.cmds." + key + ".checker_cmd"),
+			Args:           viper.GetStringSlice("testy_manager.cmds." + key + ".args"),
 		}
-		return output
+		configs = append(configs, cfg)
 	}
-	// searchIntervalStr := testy["search_interval"].(string)
-	return &Config{
-		Directories: sliceInterfacesToSliceStrings(testy["upload_dir"].([]interface{})),
-		// SearchInterval: time.Duration(searchIntervalStr),
-		Command: testy["checker_cmd"].(string),
-		Args:    sliceInterfacesToSliceStrings(testy["args"].([]interface{})),
-	}
+	return configs
 }
 
 func main() {
 	initConfig("viper-test.json")
 
-	cmds := viper.GetStringMap("testy_manager.cmds")
-	testy, _ := cmds["testy"].(map[string]interface{})
-	fmt.Println("Directories:", testy["upload_dir"])
-	fmt.Println("SearchInterval:", testy["search_interval"])
-	fmt.Println("Command:", testy["checker_cmd"])
-	fmt.Println("Args:", testy["args"])
-
-	cfg := getTestyManagerConfig()
-	fmt.Println("Cfg:", cfg)
+	cfgs := getTestyManagerConfigs()
+	fmt.Println("Cfgs:", cfgs[0])
 
 }
