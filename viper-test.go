0bdf4431cfe35096982caf29b9277c188bd0b2c4
Ricardo Angeli <rangeli@fetchrobotics.com>
2020-05-20 12:51:45 -0400

    Created viper-test

---
+++ viper-test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// Config defines the testy manager configuration
+type Config struct {
+	Directories    []string
+	SearchInterval time.Duration
+	Command        string
+	Args           []string
+}
+
+func initConfig(conf string) error {
+	viper.SetConfigFile(conf)
+	if err := viper.ReadInConfig(); err != nil {
+		return fmt.Errorf("Params file %s not found", conf)
+	}
+	return nil
+}
+
+func getTestyManagerConfig() *Config {
+	return &Config{
+		Directories:    viper.GetStringSlice("testy_manager.upload_dir"),
+		SearchInterval: viper.GetDuration("testy_manager.search_interval"),
+		Command:        viper.GetString("testy_manager.checker_cmd"),
+		Args:           viper.GetStringSlice("testy_manager.args"),
+	}
+}
+
+func main() {
+	initConfig("viper-test.json")
+	cfg := getTestyManagerConfig()
+	fmt.Println("Config:", cfg)
+}
